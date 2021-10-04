db7b4e4cb90ef9b8705f4378cafa2a231cd7d905
Hannah <hannahwoodward@hotmail.com>
2021-10-04 12:58:16 +0800

    complete

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+// question 1 (for loop)
+function writeCards(arr, event) {
+    let thankYouCards = [];
+    for (let i = 0; i < arr.length; i++) {
+        thankYouCards.push(`Thank you ${arr[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouCards
+}
+//console.log(writeCards(["Ada", "Brendan", "Ali"], "birthday"));
+
+// question 2 (while loop)
+function countDown (num) {
+    while (num > 0) {
+        console.log(num);
+        num -= 1;
+    }
+    console.log(num);
+};
+
+console.log(countDown(5));
